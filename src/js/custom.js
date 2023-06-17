b1ccf88dd49d54b5cf0a9c573243cb2c6bc666ec
gkatpal <gaurav.jagmohan.sharma@gmail.com>
2023-06-17 20:29:08 +0530

    new changes with gallery

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,2 +1,10 @@
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+console.log('hello all')
+
+$('.slick').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  });
