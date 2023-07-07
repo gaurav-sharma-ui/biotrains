b363989fd5c0dad04a24b36f8805f34c1600f671
gkatpal <gaurav.jagmohan.sharma@gmail.com>
2023-07-08 00:30:10 +0530

    services changes

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,47 +1,65 @@
 console.log('hello all')
 
 $('.slick').slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        }
-      ]
-  });
+  infinite: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 
-  $('.testimonials').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
-    prevArrow: $('.prev'),
-    nextArrow: $('.next')
-  })
+$('.testimonials').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  prevArrow: $('.prev'),
+  nextArrow: $('.next')
+})
 
-  $('.ourPartners__images').slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        }
-      ]
-  })
+$('.ourPartners__images').slick({
+  infinite: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+})
+
+$('.ourEvents__slick').slick({
+  infinite: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+})
